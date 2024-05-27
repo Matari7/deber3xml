170a1a280f3fbbac925f4c67d683b346aa33ad85
Matari7 <arimcf10@hotmail.es>
2024-05-26 20:20:54 -0500

    serverxml.js

--- serverxml.js
+++ serverxml.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const xml2js = require('xml2js');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.text({ type: 'application/xml' }));
 
@@ -34,6 +34,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en ${PORT}`);
 });
